8dfd908efc46f4941d0e963bd99c3caac4581fff
Haja <haja.joelinjatovo@emediaplace.com>
2020-03-09 16:41:53 +0300

    Dao

--- app/src/main/java/com/joelinjatovo/navette/app/dao/BaseDao.java
+++ app/src/main/java/com/joelinjatovo/navette/app/dao/BaseDao.java
@@ -3,10 +3,7 @@ package com.joelinjatovo.navette.app.dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
-import androidx.room.Query;
 import androidx.room.Update;
-
-import java.util.List;
 
 public interface BaseDao<T> {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
