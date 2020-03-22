de0e305adde12d290fb1b7eec91091737138b292
Joelinjatovo <joelinjatovo@gmail.com>
2020-03-22 14:48:59 +0300

    Home'

--- app/src/main/java/com/joelinjatovo/navette/ui/order/OrderFragment.java
+++ app/src/main/java/com/joelinjatovo/navette/ui/order/OrderFragment.java
@@ -24,16 +24,30 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.joelinjatovo.navette.R;
+import com.joelinjatovo.navette.api.models.google.GoogleDirectionResponse;
+import com.joelinjatovo.navette.api.services.GoogleApiService;
 import com.joelinjatovo.navette.database.entity.CarAndModel;
+import com.joelinjatovo.navette.database.entity.ClubAndPoint;
 import com.joelinjatovo.navette.databinding.FragmentOrderBinding;
 import com.joelinjatovo.navette.utils.Log;
 import com.joelinjatovo.navette.utils.Utils;
 import com.joelinjatovo.navette.vm.MyViewModelFactory;
 import com.joelinjatovo.navette.vm.OrderViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class OrderFragment extends Fragment implements OnMapReadyCallback {
 
@@ -51,6 +65,12 @@ public class OrderFragment extends Fragment implements OnMapReadyCallback {
     private FragmentOrderBinding mBinding;
 
     private OrderViewModel orderViewModel;
+
+    private ClubAndPoint mClubAndPoint;
+
+    private Polyline line;
+
+    ArrayList<LatLng> markerPoints;
 
     @Nullable
     @Override
@@ -101,9 +121,14 @@ public class OrderFragment extends Fragment implements OnMapReadyCallback {
                         return;
                     }
 
+                    mClubAndPoint = clubAndPoint;
+
                     Log.d(TAG, clubAndPoint.getClub().getName());
 
                     mBinding.destinationLocationTextInputEditText.setText(clubAndPoint.getClub().getName());
+
+                    getDirectionAndDistance("driving");
+
                 });
 
         orderViewModel.getRetrofitResult().observe(getViewLifecycleOwner(), listRemoteLoaderResult -> {
@@ -154,6 +179,8 @@ public class OrderFragment extends Fragment implements OnMapReadyCallback {
                                                 mLastKnownLocation.getLongitude()), 10));
 
                                 mBinding.originLocationTextInputEditText.setText(R.string.my_location);
+
+                                getDirectionAndDistance("driving");
                             }
                         } else {
                             //Log.d(TAG, "Current location is null. Using defaults.");
@@ -209,6 +236,97 @@ public class OrderFragment extends Fragment implements OnMapReadyCallback {
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_PERMISSIONS_REQUEST_CODE);
         }
+    }
+
+    private void getDirectionAndDistance(String type) {
+        if(mLastKnownLocation == null ){
+            return;
+        }
+        if(mClubAndPoint == null ){
+            return;
+        }
+
+        LatLng origin = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+        LatLng destination = new LatLng(mClubAndPoint.getPoint().getLat(), mClubAndPoint.getPoint().getLng());
+
+        String url = "https://maps.googleapis.com/maps/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        String key = getString(R.string.google_maps_key);
+
+        GoogleApiService service = retrofit.create(GoogleApiService.class);
+        Call<GoogleDirectionResponse> call = service.getDirection(key,"metric", origin.latitude + "," + origin.longitude,destination.latitude + "," + destination.longitude, type);
+        call.enqueue(new Callback<GoogleDirectionResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<GoogleDirectionResponse> call, @NonNull Response<GoogleDirectionResponse> response) {
+                try {
+                    //Remove previous line from map
+                    if (line != null) {
+                        line.remove();
+                    }
+                    // This loop will go through all the results and add marker on each location.
+                    GoogleDirectionResponse googleDirectionResponse = response.body();
+
+                    Log.e(TAG, "googleDirectionResponse = " + googleDirectionResponse);
+                    for (int i = 0; i < googleDirectionResponse.getRoutes().size(); i++) {
+                        String distance = googleDirectionResponse.getRoutes().get(i).getLegs().get(i).getDistance().getText();
+                        String time = googleDirectionResponse.getRoutes().get(i).getLegs().get(i).getDuration().getText();
+                        //mBinding.durationTextView.setText(String.format("Distance:%s, Duration:%s", distance, time));
+                        String encodedString = googleDirectionResponse.getRoutes().get(0).getOverviewPolyline().getPoints();
+                        List<LatLng> list = decodePoly(encodedString);
+                        line = mMap.addPolyline(new PolylineOptions()
+                                .addAll(list)
+                                .width(5)
+                                .color(R.color.colorAccent)
+                                .geodesic(true)
+                        );
+                    }
+                } catch (Exception e) {
+                    Log.e(TAG, "There is an error", e);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<GoogleDirectionResponse> call, @NonNull Throwable t) {
+                Log.e(TAG, t.toString(), t);
+            }
+        });
+
+    }
+    private List<LatLng> decodePoly(String encoded) {
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng( (((double) lat / 1E5)),
+                    (((double) lng / 1E5) ));
+            poly.add(p);
+        }
+
+        return poly;
     }
 
     public interface OnListFragmentInteractionListener {
