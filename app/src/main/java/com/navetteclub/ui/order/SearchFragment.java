42c1530650deb2b8d01bf78977c1f17c08880165
Joelinjatovo <joelinjatovo@gmail.com>
2020-04-08 11:43:43 +0300

    Order

--- app/src/main/java/com/navetteclub/ui/order/SearchFragment.java
+++ app/src/main/java/com/navetteclub/ui/order/SearchFragment.java
@@ -1,9 +1,13 @@
 package com.navetteclub.ui.order;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
@@ -13,20 +17,51 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.navetteclub.R;
 import com.navetteclub.databinding.FragmentSearchBinding;
 import com.navetteclub.utils.Log;
+import com.navetteclub.utils.UiUtils;
+import com.navetteclub.utils.Utils;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment  implements OnMapReadyCallback {
     private static final String TAG = SearchFragment.class.getSimpleName();
 
+    // Used in checking for runtime permissions.
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+    private static final int AUTOCOMPLETE_REQUEST_CODE = 1;
+
+    private static final float MAP_ZOOM = 20;
+
+    private GoogleMap mMap;
+
+    private Location mLastKnownLocation;
+
+    private LatLng mLocation;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
     private FragmentSearchBinding mBinding;
+
+    private Marker mMarker;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +80,8 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        setupUi();
+
         // Initialize the AutocompleteSupportFragment.
         AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
@@ -58,7 +95,8 @@ public class SearchFragment extends Fragment {
             autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
                 @Override
                 public void onPlaceSelected(@NonNull Place place) {
-                    Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                    Log.i(TAG, "Place: " + place.getName() + ", " + place.getLatLng());
+                    setLocation(place.getLatLng());
                 }
 
                 @Override
@@ -66,6 +104,117 @@ public class SearchFragment extends Fragment {
                     Log.i(TAG, "An error occurred: " + status);
                 }
             });
+        }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setupMap();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        getDeviceLocation();
+    }
+
+    private void setupUi() {
+        mBinding.myLocation.setOnClickListener(
+                v->{
+                    getDeviceLocation();
+                });
+    }
+
+    private void setupMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        // Check that the user hasn't revoked permissions by going to Settings.
+        if (Utils.requestingLocationUpdates(requireContext())) {
+            if (!checkPermissions()) {
+                requestPermissions();
+            }
+        }
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+    }
+
+    private void setLocation(LatLng latLng) {
+        mLocation = latLng;
+        if(mMap!=null){
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, MAP_ZOOM));
+        }
+
+        if(mMarker!=null){
+            mMarker.remove();
+        }
+
+        // Creating MarkerOptions
+        MarkerOptions options = new MarkerOptions();
+        options.position(latLng);
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        mMarker = mMap.addMarker(options);
+    }
+
+    private void getDeviceLocation() {
+        try {
+            if (checkPermissions()) {
+                mBinding.setIsLoading(true);
+                Task locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(requireActivity(),
+                        task -> {
+                            Log.d(TAG, "locationResult.addOnCompleteListener");
+                            mBinding.setIsLoading(false);
+                            if (task.isSuccessful()) {
+                                mLastKnownLocation = (Location) task.getResult();
+                                if (mLastKnownLocation != null) {
+                                    LatLng latLng = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                                    setLocation(latLng);
+                                }
+                            } else {
+                                Log.e(TAG, "Exception: %s", task.getException());
+                            }
+                        });
+            }
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+    private boolean checkPermissions() {
+        return  PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+    }
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+            Snackbar.make(
+                    mBinding.getRoot(),
+                    R.string.permission_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat.requestPermissions(requireActivity(),
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    REQUEST_PERMISSIONS_REQUEST_CODE);
+                        }
+                    })
+                    .show();
+        } else {
+            Log.i(TAG, "Requesting permission");
+            ActivityCompat.requestPermissions(requireActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
         }
     }
 }
