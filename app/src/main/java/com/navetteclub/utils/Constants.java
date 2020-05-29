7a19979f6442cb589d1f32b4a4e22c9e667f168f
Joelinjatovo <joelinjatovo@gmail.com>
2020-05-30 01:53:54 +0300

    Map location

--- app/src/main/java/com/navetteclub/utils/Constants.java
+++ app/src/main/java/com/navetteclub/utils/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
 
     public static final String AUTHORITY = "com.navetteclub";
 
-    public static final LatLng DEFAULT_LOCATION = new LatLng(-18.915685, 47.539389);
+    public static final LatLng DEFAULT_LOCATION = new LatLng(48.8589507,2.2770197);
 
     public static final float MAP_ZOOM = 15;
 }
